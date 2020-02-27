76387fa7d387f06e34fc47ecf7fa564f4772934f
alakdam07 <47625626+alakdam07@users.noreply.github.com>
2020-02-27 08:19:52 +0200

    Random dog generate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Loader from "react-loader-spinner";
+import "./App.css";
 
 function App() {
+  const [state, setstate] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random/5");
+      const data = await response.json();
+
+      setstate(data.message);
+    } catch (error) {
+      console.log(error, "FAILED TO FETCH");
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.map((pics, index) => {
+        return (
+          <div
+            style={{ display: "inline-flex", textAlign: "center" }}
+            key={index}
+          >
+            <div className="row">
+              <div className="col s12">
+                <div className="col s12 m6">
+                  <div
+                    className="card"
+                    style={{ width: "350px", background: "black" }}
+                  >
+                    <div className="card-image">
+                      <img
+                        src={pics}
+                        alt="img"
+                        style={{ width: "350px", height: "200px" }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <div style={{ paddingLeft: "500px" }}>
+        {!loading && (
+          <button
+            className="waves-effect waves-light btn"
+            onClick={() => fetchData()}
+          >
+            Click for more dog
+          </button>
+        )}
+        {loading && (
+          <Loader
+            type="Hearts"
+            color="#7d408c"
+            height={100}
+            width={100}
+            timeout={10000}
+          />
+        )}
+      </div>
     </div>
   );
 }
